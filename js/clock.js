578b93e14918762a3c6200591bfe3bf5d0f2732d
joy <leejoys.lab@gmail.com>
2023-05-09 22:18:15 +0900

    icon 수정

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,12 @@
-const clock = document.querySelector("#clock");
+const clock = document.getElementById("clock");
+const clocks = document.querySelector(".clock");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}`;
+  clocks.innerText = `${hours}:${String(minutes - 1).padStart(2, "0")}`;
 }
 
 getClock();
-setInterval(getClock, 1000);
